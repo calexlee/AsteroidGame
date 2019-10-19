b556b6a0b3ba0ea2601fe3e7f9a40aab641a2518
Alex <cal368@cornell.edu>
2019-10-18 22:29:19 -0400

    Created Boss
    Implemented movement, collisions, shooting and beams, still need to
    implement proper scoring methods, also changes aliens to have a 50-50 on
    which direction to fly

--- AsteroidGame/TestGame/Main/Alien.py
+++ AsteroidGame/TestGame/Main/Alien.py
@@ -8,8 +8,9 @@ TODO: make aliens more interesting
 
 import random 
 import arcade
+from PIL.ImageChops import screen
 
-def createAlien(self, alien_list, numOfAliens, alienType, SCREEN_HEIGHT, SCREEN_WIDTH):
+def createAlien(self, alien_list, numOfAliens, alienType, screen_height, screen_width):
     
     ''' Creates a new alien and adds it to the alien list'''
     
@@ -29,34 +30,69 @@ def createAlien(self, alien_list, numOfAliens, alienType, SCREEN_HEIGHT, SCREEN_
     
     if spawn == 1:
         alien.center_x = 10
-        alien.center_y = SCREEN_HEIGHT - 1
+        alien.center_y = screen_height - 1
     elif spawn == 2:
-        alien.center_x = SCREEN_WIDTH-10
-        alien.center_y = SCREEN_HEIGHT - 1
+        alien.center_x = screen_width-10
+        alien.center_y = screen_height - 1
     elif spawn == 3:
-        alien.center_x = SCREEN_WIDTH/2
-        alien.center_y = SCREEN_HEIGHT - 1
+        alien.center_x = screen_width/2
+        alien.center_y = screen_height - 1
     
     # Movement Pattern
     if alien.type == 1:
-        alien.change_x = 3
+        if random.randint(1,2) == 1:
+            alien.change_x = 3
+        else: 
+            alien.change_x = -3
         alien.change_y = -0.5
     
     if alien.type == 2:
-        alien.change_x = 1
+        if random.randint(1,2) == 1:
+            alien.change_x = 1
+        else: 
+            alien.change_x = -1
         alien.change_y = -0.5
     
     if alien.type == 3:
+        if random.randint(1,2) == 1:
+            alien.change_x = 2
+        else: 
+            alien.change_x = -2
         alien.change_y = -0.25
-        alien.change_x = 2
         
     if alien.type == 4:
+        if random.randint(1,2) == 1:
+            alien.change_x = 0.5
+        else: 
+            alien.change_x = -0.5
         alien.change_y = -0.1
-        alien.change_x = 0.5
                 
     # Adding to List
     alien_list.append(alien)
     numOfAliens = numOfAliens + 1
     
     return alien_list, numOfAliens
+
+def createBoss(self,boss_list,screen_height,screen_width):
+    ''' Creates a boss alien '''
+    
+    boss_size = 5
+    boss = arcade.Sprite("Resources/boss.png",boss_size)
+    boss.health = 1000
+    boss.beamTimer = 1000
+    boss.beamActive = False
+    boss.shootTimer = 50
+    
+    boss.center_x = screen_width/2
+    boss.center_y = screen_height - 80
+    
+    boss.change_x = 0
+    boss.change_y = 0
+    
+    boss.height = boss.height/1.5
+    boss.width = boss.width*1.5
+    
+    boss_list.append(boss)
+    
+    return boss_list
     
--- AsteroidGame/TestGame/Main/Game.py
+++ AsteroidGame/TestGame/Main/Game.py
@@ -16,12 +16,13 @@ SCREEN_HEIGHT = 768
 SPRITE_SCALING_PLAYER = 1.25
 MOVEMENT_SPEED = 5
 PLAYER_FLOAT = 0.1
-START_ASTEROID = 4
+START_ASTEROID = 1
 SPRITE_MAX_SCALING_ASTEROID = 2
 MAX_ASTEROID_SPEED = 1
 SPRITE_SCALING_BOLT = 0.25
 BOLT_SPEED = 7
 SPRITE_SCALING_LASER = 0.2
+SPRITE_SCALING_BEAM = 2
 
 # Game States
 TITLE_SCREEN = 0
@@ -52,13 +53,16 @@ class MyGame(arcade.Window):
         self.laser_list = arcade.SpriteList()
         self.alien_list = arcade.SpriteList()
         self.powerup_list = arcade.SpriteList()
+        self.boss_list = arcade.SpriteList()
+        self.beam_list = arcade.SpriteList()
         
         # Initial lives, Score and More
         self.lives = 3
-        self.score = 0
+        self.score = 1000
         self.numOfAsteroids = 0
         self.numOfAliens = 0
         self.numOfPow = 0
+        self.numOfBoss = 0
         self.laser = False
         self.bb = 0
         self.pow = False
@@ -113,6 +117,8 @@ class MyGame(arcade.Window):
         self.alien_list.draw()
         self.laser_list.draw()
         self.powerup_list.draw()
+        self.boss_list.draw()
+        self.beam_list.draw()
         
         # Drawing of still objects
         start_x = 60
@@ -148,14 +154,16 @@ class MyGame(arcade.Window):
             hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.asteroid_list)
             bolt_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.bolt_list)
             alien_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.alien_list)
+            boss_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.boss_list)
+            beam_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.beam_list)
             
-            
-            if hit_list or bolt_hit_list or alien_hit_list:
+            # Checking player collisions
+            if hit_list or bolt_hit_list or alien_hit_list or boss_hit_list or beam_hit_list:
                 if self.lives > 1:
                     self.player_sprite.center_x = SCREEN_WIDTH/2
                     self.player_sprite.center_y = SCREEN_HEIGHT/2
                     self.lives = self.lives - 1
-                    self.score = self.score - 100
+                    self.score = self.score - 10
                 else:
                     self.lives = 0
                     self.player_sprite.set_texture(1)
@@ -168,7 +176,8 @@ class MyGame(arcade.Window):
             if self.numOfPow < 1 and random.randint(1,10000) < 50:
                 powType = 1
                 self.numOfPow = self.numOfPow + 1
-                self.powerup_list = Main.Powerup.createPowerUp(self,self.powerup_list, powType, SCREEN_HEIGHT, SCREEN_WIDTH)
+                self.powerup_list = Main.Powerup.createPowerUp\
+                                    (self,self.powerup_list, powType, SCREEN_HEIGHT, SCREEN_WIDTH)
             
             pow_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.powerup_list)
             
@@ -185,69 +194,134 @@ class MyGame(arcade.Window):
                         self.player_sprite.set_texture(3)
                         self.bb = 4
                         self.pow = True
-    
-            # Creation of Aliens (ultimately want to put on a timer)
-            if self.score < 250:
-                spawn = random.randint(1,200)
-            elif self.score < 500:
-                spawn = random.randint(1,100)
-            else: 
-                spawn = random.randint(1,50)
             
-            if spawn == 1:
-                alienType = random.randint(1,4)
-                self.alien_list, self.numOfAliens = Main.Alien.createAlien(MyGame, self.alien_list, self.numOfAliens, alienType, SCREEN_HEIGHT, SCREEN_WIDTH)
-            
-            # Alien Movement
-            for alien in self.alien_list:
-                alien.center_x = alien.center_x + alien.change_x
-                alien.center_y = alien.center_y + alien.change_y
+            # Boss summoning
+            if self.score > 1000 and self.numOfBoss < 1:
+                self.boss_list = Main.Alien.createBoss(MyGame, self.boss_list, SCREEN_HEIGHT, SCREEN_WIDTH)
+                self.numOfBoss = 1
+                if self.numOfAliens > 0:
+                    for alien in self.alien_list:
+                        alien.kill()
                 
-                #Alien 3 follow pattern
-                if alien.type == 3:
-                    if self.player_sprite.center_x > alien.center_x:
-                        alien.change_x = 2
-                    elif self.player_sprite.center_x < alien.center_x:
-                        alien.change_x = -2
+            for boss in self.boss_list:
+                boss.center_x =  boss.center_x + boss.change_x
+                boss.center_y = boss.center_y + boss.change_y
+                boss.beamTimer = boss.beamTimer - 1
                 
-#                 # Aliens collide with asteroid
-#                 alien_hit_list = arcade.check_for_collision_with_list(alien, self.asteroid_list)
-#                 if alien_hit_list:
-#                     #TODO: Add result of alien death
-#                     alien.kill()
+                if self.player_sprite.center_x > boss.center_x:
+                            boss.change_x = 0.2
+                elif self.player_sprite.center_x < boss.center_x:
+                            boss.change_x = -0.2
                 
-                # Alien shooting
-                degreeOfSpace = 3
-                if alien.center_x < self.player_sprite.center_x + degreeOfSpace and alien.center_x > self.player_sprite.center_x - degreeOfSpace and alien.center_y > self.player_sprite.center_y:
-                    if alien.type <= 3:
-                        bolt = arcade.Sprite("Resources/alienbolt.png", SPRITE_SCALING_BOLT) # change to alien bolt
-                        bolt.type = 0
-                        bolt.center_x = alien.center_x 
-                        bolt.center_y = alien.center_y - 20
-                        bolt.change_x = 0
-                        bolt.change_y = -BOLT_SPEED*.65
-                        self.bolt_list.append(bolt)
-                    if alien.type == 4:
-                        bolt = arcade.Sprite("Resources/alienbolt.png", 5*SPRITE_SCALING_BOLT) # change to alien bolt
-                        bolt.type = 1
-                        bolt.center_x = alien.center_x 
-                        bolt.center_y = alien.center_y - 60
-                        bolt.change_x = 0
-                        bolt.change_y = -BOLT_SPEED*.35
-                        self.bolt_list.append(bolt)
-                
-                # Off Screen
-                if alien.center_x > SCREEN_WIDTH:
-                    alien.center_x = 1
-                
-                if alien.center_x < 0:
-                    alien.center_x = SCREEN_WIDTH - 1
-                
-                if alien.center_y > SCREEN_HEIGHT:
-                    alien.kill()
+                # Boss Beaming
+                if boss.beamTimer < 500 and not boss.beamActive:
+                    beam = arcade.Sprite("Resources/alienbolt.png", SPRITE_SCALING_BEAM) # Change to beam
+                    beam.height = SCREEN_HEIGHT / 1.2          
+                    beam.center_x = boss.center_x
+                    beam.center_y = boss.center_y - 350
+                    boss.beamActive = True
+                    self.beam_list.append(beam)
+                elif boss.beamTimer < 500 and boss.beamTimer > 0:
+                    for beam in self.beam_list:
+                        beam.center_x = boss.center_x
+                        beam.center_y = boss.center_y - 350
+                        boss.beamTimer = boss.beamTimer - 1 
+                elif boss.beamTimer <= 0:
+                    for beam in self.beam_list:
+                        beam.kill()
+                    boss.beamActive = False
+                    boss.beamTimer = 1000
                     
-                if alien.center_y < 0:
-                    alien.kill()
+                # Boss Shooting
+                if boss.shootTimer <= 0:
+                        for i in range(1,6):
+                            bolt = arcade.Sprite("Resources/alienbolt.png", SPRITE_SCALING_BOLT) 
+                            bolt.type = 0
+                            bolt.center_x = boss.center_x - 100 + (30*i)
+                            bolt.center_y = boss.center_y - 60
+                            if i == 1:
+                                bolt.change_x = -2
+                            elif i == 2:
+                                bolt.change_x = -1
+                            elif i == 3:
+                                bolt.change_x = 0
+                            elif i == 4:
+                                bolt.change_x = 1
+                            elif i == 5:
+                                bolt.change_x = 2
+                                                                
+                            bolt.change_y = -BOLT_SPEED*.35
+                            self.bolt_list.append(bolt)
+                            boss.shootTimer = 50
+                else:
+                    boss.shootTimer = boss.shootTimer - 1
+                
+            if self.numOfBoss < 1:
+                # Creation of Aliens (ultimately want to put on a timer)
+                if self.score < 250:
+                    spawn = random.randint(1,200)
+                elif self.score < 500:
+                    spawn = random.randint(1,100)
+                else: 
+                    spawn = random.randint(1,50)
+                
+                if spawn == 1:
+                    alienType = random.randint(1,4)
+                    self.alien_list, self.numOfAliens = Main.Alien.createAlien\
+                                        (MyGame, self.alien_list, self.numOfAliens, alienType, SCREEN_HEIGHT, SCREEN_WIDTH)
+                
+                # Alien Movement
+                for alien in self.alien_list:
+                    alien.center_x = alien.center_x + alien.change_x
+                    alien.center_y = alien.center_y + alien.change_y
+                    
+                    #Alien 3 follow pattern
+                    if alien.type == 3:
+                        if self.player_sprite.center_x > alien.center_x:
+                            alien.change_x = 2
+                        elif self.player_sprite.center_x < alien.center_x:
+                            alien.change_x = -2
+                    
+    #                 # Aliens collide with asteroid
+    #                 alien_hit_list = arcade.check_for_collision_with_list(alien, self.asteroid_list)
+    #                 if alien_hit_list:
+    #                     #TODO: Add result of alien death
+    #                     alien.kill()
+                    
+                    # Alien shooting
+                    degreeOfSpace = 3
+                    if alien.center_x < self.player_sprite.center_x + degreeOfSpace \
+                    and alien.center_x > self.player_sprite.center_x - degreeOfSpace \
+                    and alien.center_y > self.player_sprite.center_y:
+                        if alien.type <= 3:
+                            bolt = arcade.Sprite("Resources/alienbolt.png", SPRITE_SCALING_BOLT) 
+                            bolt.type = 0
+                            bolt.center_x = alien.center_x 
+                            bolt.center_y = alien.center_y - 20
+                            bolt.change_x = 0
+                            bolt.change_y = -BOLT_SPEED*.65
+                            self.bolt_list.append(bolt)
+                        if alien.type == 4:
+                            bolt = arcade.Sprite("Resources/alienbolt.png", 5*SPRITE_SCALING_BOLT) 
+                            bolt.type = 1
+                            bolt.center_x = alien.center_x 
+                            bolt.center_y = alien.center_y - 60
+                            bolt.change_x = 0
+                            bolt.change_y = -BOLT_SPEED*.35
+                            self.bolt_list.append(bolt)
+                    
+                    # Off Screen
+                    if alien.center_x > SCREEN_WIDTH:
+                        alien.center_x = 1
+                    
+                    if alien.center_x < 0:
+                        alien.center_x = SCREEN_WIDTH - 1
+                    
+                    if alien.center_y > SCREEN_HEIGHT:
+                        alien.kill()
+                        
+                    if alien.center_y < 0:
+                        alien.kill()
             
             # Asteroid Movement
             for asteroid in self.asteroid_list:
@@ -285,6 +359,19 @@ class MyGame(arcade.Window):
                     bolt.kill()
                     self.numOfAliens = self.numOfAliens - 1
                     self.score = self.score + 50
+                
+                # Collision with boss
+                bolt_hit_list = arcade.check_for_collision_with_list(bolt,self.boss_list)
+                for boss_hit in bolt_hit_list:
+                    if boss_hit.health > 0:
+                        if bolt.type == 0:
+                            boss_hit.health = boss_hit.health - 1
+                        elif bolt.type == 75:
+                            boss_hit.health = boss_hit.health - 75
+                    elif boss_hit.health <= 0:
+                        boss_hit.kill()
+                        self.numOfBoss = self.numOfBoss - 1
+                    bolt.kill()
                 
                 # Collision with asteroid
                 bolt_hit_list = arcade.check_for_collision_with_list(bolt,self.asteroid_list)
@@ -331,7 +418,42 @@ class MyGame(arcade.Window):
                 if bolt.center_x > SCREEN_WIDTH or bolt.center_x < 0 \
                 or bolt.center_y > SCREEN_HEIGHT or bolt.center_y < 0:
                     bolt.kill()
-                    
+            
+            for beam in self.beam_list:
+                # Collision with asteroid
+                laser_hit_list = arcade.check_for_collision_with_list(beam,self.asteroid_list)
+                for asteroid_hit in laser_hit_list:
+                        
+                    if asteroid_hit.health > 1:
+                        asteroid_hit.health = asteroid_hit.health - 1
+                        self.score = self.score + 1
+                        if asteroid_hit.health <= .25*(5*SPRITE_MAX_SCALING_ASTEROID):
+                            asteroid_hit.set_texture(3)
+                            asteroid_hit._set_scale(asteroid_hit.scale)
+                        elif asteroid_hit.health <= .5*(5*SPRITE_MAX_SCALING_ASTEROID):
+                            asteroid_hit.set_texture(2)
+                            asteroid_hit._set_scale(asteroid_hit.scale)
+                        elif asteroid_hit.health <= .75*(5*SPRITE_MAX_SCALING_ASTEROID):
+                            asteroid_hit.set_texture(1)
+                            asteroid_hit._set_scale(asteroid_hit.scale)
+                    else:
+                        # Splits asteroid into two unless it is of smallest size
+                        if asteroid_hit.scale > SPRITE_MAX_SCALING_ASTEROID/2:
+                            x1 = asteroid_hit.center_x + asteroid_hit.collision_radius/2
+                            y1 = asteroid_hit.center_y + asteroid_hit.collision_radius/2
+                            x2 = asteroid_hit.center_x - asteroid_hit.collision_radius/2
+                            y2 = asteroid_hit.center_y - asteroid_hit.collision_radius/2
+                            
+                            self.asteroid_list = Main.Asteroid.createMiniAsteroid(MyGame, self.asteroid_list,
+                                 x1, y1, SPRITE_MAX_SCALING_ASTEROID, MAX_ASTEROID_SPEED)
+                                
+                            self.asteroid_list = Main.Asteroid.createMiniAsteroid(MyGame, self.asteroid_list,
+                                x2, y2, SPRITE_MAX_SCALING_ASTEROID, MAX_ASTEROID_SPEED)
+                            self.numOfAsteroids = self.numOfAsteroids + 2
+                                
+                        asteroid_hit.kill()
+            
+            
             for laser in self.laser_list:
                 # Movement
                 laserDistFromPlayer = laser.height/2 + 25
@@ -364,6 +486,21 @@ class MyGame(arcade.Window):
                     self.score = self.score + 50
                     if laser.health <= 0:
                         laser.kill()
+                
+                # Collision with boss
+                laser_hit_list = arcade.check_for_collision_with_list(laser,self.alien_list)
+                for boss_hit in laser_hit_list:
+                    if boss_hit.health > 1:
+                        boss_hit.health = boss_hit.health - 1
+                        laser.health = laser.health - 1
+                        if laser.health <= 0:
+                            laser.kill()
+                    else:
+                        boss_hit.kill()
+                        laser.health = laser.health - 1
+                        if laser.health <= 0:
+                            laser.kill()
+                        self.numOfBoss = self.numOfBoss - 1
                 
                 # Collision with asteroid
                 laser_hit_list = arcade.check_for_collision_with_list(laser,self.asteroid_list)
